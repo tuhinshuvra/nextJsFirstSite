b4bdd7987613af2a4d12a63f16d174680d590fae
tuhinshuvra <tuhinshuvra18@gmail.com>
2023-07-23 14:11:30 +0600

    added some update

--- src/pages/album.js
+++ src/pages/album.js
@@ -1,19 +1,29 @@
 import Image from 'next/image';
 import React from 'react';
+import NextImg from '../assets/Getting-Started-with-NextJS-Inside.webp'
 
 const AlbumPage = () => {
     return (
         <div>
             {/* <img
-                src="https://www.pngkey.com/png/detail/147-1473883_404-error-404-not-found-png.png"
+                src="https://soshace.com/wp-content/uploads/2019/10/Getting-Started-with-NextJS-Inside.jpg"
                 alt=""
                 width="100%"
             /> */}
             <Image
-                src="https://www.pngkey.com/png/detail/147-1473883_404-error-404-not-found-png.png"
+                src="https://soshace.com/wp-content/uploads/2019/10/Getting-Started-with-NextJS-Inside.jpg"
                 alt=""
                 width={500}
                 height={500}
+                layout='responsive'
+            ></Image>
+
+            <Image
+                src={NextImg}
+                alt=""
+                width={500}
+                height={500}
+                layout='responsive'
             ></Image>
         </div >
     );
